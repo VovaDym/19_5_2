c42fc980d2365e41315f7e1c4c6b5dfe360be66a
VovaDym <77827028+VovaDym@users.noreply.github.com>
2022-01-20 12:47:42 +0300

    19.5.2

---
+++ main.cpp
@@ -0,0 +1,37 @@
+//Что нужно сделать
+//Разработайте простейший просмотрщик текстовых файлов. В начале программы пользователь вводит путь к текстовому файлу,
+// который требуется открыть и просмотреть. Внутренности файла необходимо вывести в стандартный вывод. Программа должна
+// работать для любых текстовых файлов в формате TXT, но вы можете создать и свой отдельный файл для теста. Главное,
+// чтобы файл был англоязычным, дабы избежать проблем с кодировками.
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+
+    static char  buff[1024][1024];
+
+    std::ifstream file;
+    std::string path;
+    std::cout << "Enter the path to the file: " << std::endl;
+    std::cin >> path;
+    file.open(path);
+    if(!file.is_open())
+    {
+        std::cout << "Error!";
+    }
+    else
+    {
+        std::cout << "File is open: " << std::endl;
+
+        int i = 0;
+        while (!file.eof())
+        {
+            file.getline(buff[i],sizeof(buff));
+            std::cout << buff[i] << std::endl;
+            ++i;
+        }
+    }
+    file.close();
+
+}
